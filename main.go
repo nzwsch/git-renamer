5df698ab8ece7b6490570f372768239f133d7dfe
Seiichi Yonezawa <nzwsch@gmail.com>
2025-05-11 16:30:31 +0900

    Update main.go to use home directory for path listing and improve error handling

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	dirs, err := listAllPaths(".")
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Failed to get home directory:", err)
+		return
+	}
 
+	dirs, err := listAllPaths(homeDir)
 	if err != nil {
 		fmt.Println("Error listing paths:", err)
 		return
 	}
 
-	fmt.Println("dirs:", dirs)
+	for _, dir := range dirs {
+		fmt.Println(dir)
+	}
 }
