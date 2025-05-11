67816d36e2aab6901c5a632f24372c6dca89ef94
Seiichi Yonezawa <nzwsch@gmail.com>
2025-05-11 17:06:31 +0900

    Add functionality to retrieve and display the first commit date for each git directory

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -20,6 +23,24 @@ func main() {
 
 	dirs = onlyGitDirs(dirs)
 	for _, dir := range dirs {
-		fmt.Println(dir)
+		if err := os.Chdir(dir); err != nil {
+			fmt.Println("Failed to change directory:", err)
+			return
+		}
+
+		cmd := exec.Command("git", "log", "--reverse", "--max-parents=0", "HEAD", "--format=%ci")
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Error executing git command:", err)
+			return
+		}
+		scanner := bufio.NewScanner(strings.NewReader(string(output)))
+		if scanner.Scan() {
+			dateStr := scanner.Text()
+			dateStr = strings.TrimSpace(dateStr)
+			fmt.Println("First commit date:", dateStr)
+		} else {
+			fmt.Println("No commit found")
+		}
 	}
 }
